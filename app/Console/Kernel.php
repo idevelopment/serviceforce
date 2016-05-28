0bc4f428784d80cfb424ddb456e01b63f982cb14
Tjoosten <Tjoosten3@gmail.com>
2016-05-29 01:43:50 +0200

    Fix #13 -> fix backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,5 +26,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('backup:clean')->daily()->at('01:00');
+        $schedule->command('backup:run')->daily()->at('02:00');
+        $schedule->command('backup:monitor')->daily()->at('03:00');
     }
 }
