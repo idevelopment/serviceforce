131478573d5b735d6773e47f32ae717b446fbd05
Tjoosten <Tjoosten3@gmail.com>
2016-05-29 03:54:46 +0200

    Update on controllers. Now my bed needs me

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Customers;
 
+/**
+ * Class CustomersController
+ * @package App\Http\Controllers
+ *
+ * TODO: needs phpunit tests.
+ */
 class CustomersController extends Controller
 {
     /**
@@ -15,6 +21,7 @@ class CustomersController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('lang');
     }
     /**
      * The customer index view.
@@ -27,4 +34,51 @@ class CustomersController extends Controller
         return view('customers/index', $data);
     }
 
+    public function edit($id)
+    {
+        return view('customers.edit')
+    }
+
+    /**
+     * Stroe a new customer to the database.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store()
+    {
+        return redirect()->back(302);
+    }
+
+    /**
+     * Create a new customer.
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('customers.create');
+    }
+
+    /**
+     * Update a specific customer,
+     *
+     * @param  int $id the customer id in the database.
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update($id)
+    {
+        return redirect()->back(302);
+    }
+
+    /**
+     * Soft delete a user in the database.
+     *
+     * @param  int $id the customer id in the database.
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy($id)
+    {
+        // TODO: Implement soft delete on the database.
+        return redirect()->back(302);
+    }
 }
