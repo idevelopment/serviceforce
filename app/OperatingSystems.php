f68d7da81617a2be13055e74cf6a281ecc665897
Tjoosten <Tjoosten3@gmail.com>
2016-05-31 07:31:27 +0200

    Add hidden en fillable to the OS model

--- app/OperatingSystems.php
+++ app/OperatingSystems.php
@@ -4,7 +4,23 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class OperatingSystems
+ * @package App
+ */
 class OperatingSystems extends Model
 {
-    //
+    /**
+     * Mass-assign fields.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+    /**
+     * Hidden database field for the view.
+     *
+     * @var array
+     */
+    protected $hidden = ['created_at', 'updated_at'];
 }
