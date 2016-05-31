792fbbac57922cafed3a3d6ab018b912aa27b5d6
Tjoosten <Tjoosten3@gmail.com>
2016-05-31 19:22:06 +0200

    Fix #12 language middlware implementation

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -15,6 +15,7 @@ class CustomersController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('lang');
     }
 
     /**
--- app/Http/Controllers/ServersController.php
+++ app/Http/Controllers/ServersController.php
@@ -17,6 +17,7 @@ class ServersController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+		$this->middleware('lang');
     }
 
     /**
