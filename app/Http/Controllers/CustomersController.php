6765771fd44fe6a8e729259b6baeb70e47db3e5d
Tjoosten <Tjoosten3@gmail.com>
2016-06-02 04:08:44 +0200

    Add Customer update controller

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -41,6 +41,20 @@ class CustomersController extends Controller
     }
 
     /**
+     * Update a user in the database.
+     *
+     * @param  Requests\CostumerValidator $input
+     * @param  int $id the customer id in the database.
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(Requests\CostumerValidator $input, $id)
+    {
+        Customers::find($id)->update($input->except('_token'));
+        session()->flash('message', 'Costumer updated.');
+        return redirect()->back(302);
+    }
+
+    /**
      * Register a new customer.
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
@@ -53,7 +67,7 @@ class CustomersController extends Controller
     /**
      * Store a new costumer in the database.
      *
-     * @param  Requests\CustomerValidator $input
+     * @param  Requests\CostumerValidator $input
      * @return \Illuminate\Http\RedirectResponse
      */
     public function store(Requests\CostumerValidator $input)
