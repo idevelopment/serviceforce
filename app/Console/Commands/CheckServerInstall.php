7ea8385ae47ffa6c7d74943d38db67347ef1f53b
Glenn Hermans <glhermans@gmail.com>
2016-08-13 21:44:52 +0200

    Fixed merge conflict

--- app/Console/Commands/CheckServerInstall.php
+++ app/Console/Commands/CheckServerInstall.php
@@ -48,7 +48,6 @@ class CheckServerInstall extends Command
      */
     public function handle()
     {
-<<<<<<< HEAD
       $apiKey   = config('ServiceForge.leaseweb.apikey');
       $apiUrl   = 'https://api.leaseweb.com/v1';
 
@@ -98,40 +97,3 @@ class CheckServerInstall extends Command
       }
      }
   }
-=======
-        $apiKey = config('ServiceForge.leaseweb.apikey');
-        $apiUrl = 'https://api.leaseweb.com/v1';
-
-        // Get all servers that need to execute provisioning
-        $PayAsYouGo = Servers::where('status', 'progress')
-            ->orderBy('id', 'asc')
-            ->get();
-
-        foreach ($PayAsYouGo as $order) {
-
-            $serverId = $order->bareMetalId;
-
-            $installResource = "/bareMetals/$serverId/installationStatus";
-
-            $checkInstall = curl_init();
-            curl_setopt($checkInstall, CURLOPT_URL, $apiUrl . $installResource);
-            curl_setopt($checkInstall, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($checkInstall, CURLOPT_HTTPHEADER, array("X-Lsw-Auth: $apiKey"));
-            $verify = curl_exec($checkInstall);
-            $data = json_decode($verify, true);
-
-            if ($data["installationStatus"]["code"] = "1000") {
-                Servers::where('id', $order->id)
-                    ->where('status', 'progress')
-                    ->update(['status' => 'completed', 'bareMetalId' => $order->bareMetalId]);
-            } else {
-                Mail::send('emails.serverInstall', $data, function ($message) {
-                    $message->from('provisioning@idevelopment.be', 'iDevelopment Provisioning');
-                    $message->subject("Checking installation process - $order->bareMetalId ");
-                    $message->to('support@idevelopment.be');
-                });
-            }
-        }
-    }
-}
->>>>>>> 90a021b03ba38fa9bd2e883150fc7382cfa40b77
