634bd71df5181c7eb1f4698cc3c78f4ac19d2e63
Tjoosten <Tjoosten3@gmail.com>
2016-08-14 23:09:20 +0200

    Cleanup and docblocks -> IPMController

--- app/Http/Controllers/IPMController.php
+++ app/Http/Controllers/IPMController.php
@@ -6,38 +6,48 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+/**
+ * Class IPMController
+ * @package App\Http\Controllers
+ */
 class IPMController extends Controller
 {
-  /**
-   * CustomersController constructor.
-   */
-  public function __construct()
-  {
-      $this->middleware('auth');
-      $this->middleware('lang');
-  }
+    /**
+     * CustomersController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('lang');
+    }
 
-  public function index(){
-    $resource = '/ips';
-    $apiKey   = config('ServiceForge.leaseweb.apikey');
-    $apiUrl   = 'https://api.leaseweb.com/v1';
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        $resource = '/ips';
+        $apiKey = config('ServiceForge.leaseweb.apikey');
+        $apiUrl = 'https://api.leaseweb.com/v1';
 
-    $queryIp = curl_init();
-    curl_setopt($queryIp, CURLOPT_URL, $apiUrl . $resource);
-    curl_setopt($queryIp, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($queryIp, CURLOPT_HTTPHEADER, array("X-Lsw-Auth: $apiKey"));
-    $output = curl_exec($queryIp);
+        $queryIp = curl_init();
+        curl_setopt($queryIp, CURLOPT_URL, $apiUrl . $resource);
+        curl_setopt($queryIp, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($queryIp, CURLOPT_HTTPHEADER, array("X-Lsw-Auth: $apiKey"));
+        $output = curl_exec($queryIp);
 
-    $data["output"] = json_decode($output);
+        $data["output"] = json_decode($output);
 
-    return view('ips.index', $data);
-  }
+        return view('ips.index', $data);
+    }
 
-  public function whois($id)
-   {
-
-     $data["lookup"] = $id;
-
-     return view("ips/whois", $data);
-  }
+    /**
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function whois($id)
+    {
+        $data["lookup"] = $id;
+        return view("ips/whois", $data);
+    }
 }
