6463afa6cee934ec295e1b18d04feb2bc69e20bb
Tjoosten <Tjoosten3@gmail.com>
2016-08-15 22:01:22 +0200

    Fix #67 Add password routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,6 +20,14 @@ Route::post('login', 'Auth\AuthController@login');
 Route::get('/', 'HomeController@index');
 Route::get('/home', 'HomeController@index');
 
+// Password reset link request routes...
+Route::get('password/email', 'Auth\PasswordController@getEmail');
+Route::post('password/email', 'Auth\PasswordController@postEmail');
+
+// Password reset routes...
+Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
+Route::post('password/reset', 'Auth\PasswordController@postReset');
+
 // Profile
 Route::get('/profile', 'ProfileController@index')->name('profile.index');
 Route::post('/profile/update/info', 'ProfileController@storeInformation')->name('profile.update.information');
